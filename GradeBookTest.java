14ac6d3e63c15639aa2cfc058d537b1c2104dca2
dyw123 <55104757+dyw123@users.noreply.github.com>
2021-03-13 20:53:04 -0500

    Add files via upload

---
+++ GradeBookTest.java
@@ -0,0 +1,44 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GradeBookTest {
+	  private GradeBook g1;
+	@BeforeEach
+	void setUp() throws Exception {
+		g1 = new GradeBook(5);
+	       g1.addScore(50.0);
+	       g1.addScore(75.0);
+	       }
+
+	@AfterEach
+	void tearDown() throws Exception 
+	{
+		
+		g1 = null;
+	}
+
+	@Test
+	void testAddScore() {
+		assertTrue((g1.toString()).equals("50.0 75.0 "));  
+	     assertEquals(2, g1.getScoreSize(), 0.001);
+	}
+
+	@Test
+	void testSum() {
+	    assertEquals(125, g1.sum(), .0001);
+	}
+
+	@Test
+	void testMinimum() {
+		 assertEquals(50.0, g1.minimum(), .0001);
+	}
+
+	@Test
+	void testFinalScore() {
+	    assertEquals(75.0, g1.finalScore(), .0001);
+	}
+
+}
